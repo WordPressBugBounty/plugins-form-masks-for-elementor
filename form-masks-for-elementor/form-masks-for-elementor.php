43e50e2dd8301b152bf2686ac070c1190269090c
Sean Murphy <sean@iamseanmurphy.com>
2025-09-11 11:17:04 +0000

    2.5.5

--- form-masks-for-elementor/form-masks-for-elementor.php
+++ form-masks-for-elementor/form-masks-for-elementor.php
@@ -5,7 +5,7 @@
  * Description: Form Input Masks for Elementor Form creates a custom control in the field advanced tab for customizing your fields with masks. This plugin requires Elementor Pro (Form Widget).
  * Author: Cool Plugins
  * Author URI: https://coolplugins.net/?utm_source=fim_plugin&utm_medium=inside&utm_campaign=author_page&utm_content=plugins_list
- * Version: 2.5.4
+ * Version: 2.5.5
  * Requires at least: 5.5
  * Requires PHP: 7.4
  * Text Domain: form-masks-for-elementor
@@ -22,7 +22,7 @@
 	exit();
 }
 
-define( 'FME_VERSION', '2.5.4' );
+define( 'FME_VERSION', '2.5.5' );
 define( 'FME_FILE', __FILE__ );
 define( 'FME_PLUGIN_BASE', plugin_basename( FME_FILE ) );
 define( 'FME_PHP_MINIMUM_VERSION', '7.4' );
@@ -53,8 +53,13 @@ class Form_Masks_For_Elementor {
         if ( $this->check_requirements() ) {
             $this->initialize_plugin();
             add_action( 'init', array( $this, 'text_domain_path_set' ) );
-            add_action( 'init', array( $this, 'is_compatible' ) );
 			add_action( 'activated_plugin', array( $this, 'fme_plugin_redirection' ) );
+
+            add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'fme_pro_plugin_demo_link' ) );
+
+            add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'fme_plugin_settings_link' ) );
+
+			
 
 			add_filter( 'plugin_row_meta', [ $this, 'plugin_row_meta' ], 10, 2 );
 
@@ -65,16 +70,6 @@ class Form_Masks_For_Elementor {
     }
 
     public function plugin_loads(){
-
-        if ( ! is_plugin_active( 'elementor-pro/elementor-pro.php' ) && ! is_plugin_active( 'pro-elements/pro-elements.php' ) ) {
-				return false;
-			}
-
-
-            add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'fme_pro_plugin_demo_link' ) );
-
-            add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'fme_plugin_settings_link' ) );
-
 
 		if(!class_exists('CPFM_Feedback_Notice')){
 			require_once FME_PLUGIN_PATH . 'admin/feedback/cpfm-common-notice.php';
@@ -145,10 +140,6 @@ class Form_Masks_For_Elementor {
 
 	public function fme_plugin_redirection($plugin){
 
-        if ( ! is_plugin_active( 'elementor-pro/elementor-pro.php' ) ) {
-				return false;
-			}
-
 		if ( is_plugin_active( 'cool-formkit-for-elementor-forms/cool-formkit-for-elementor-forms.php' ) ) {
 			return false;
 		}
@@ -156,45 +147,6 @@ class Form_Masks_For_Elementor {
 		if ( $plugin == plugin_basename( __FILE__ ) ) {
 			exit( wp_redirect( admin_url( 'admin.php?page=cool-formkit' ) ) );
 		}	
-	}
-
-    /**
-	* Check if Elementor Pro is installed or activated
-	*/
-	public function is_compatible() {
-		add_action( 'admin_init', array( $this, 'is_elementor_pro_exist' ) );
-	}
-
-    /**
-	* Function used to deactivate the plugin if Elementor Pro does not exist
-	*/
-    public function is_elementor_pro_exist() {
-		if (
-			is_plugin_active('elementor-pro/elementor-pro.php')
-		) {
-			return true; // At least one plugin is active, the country code plugin can run.
-		}
-		
-		// If neither plugin is active, show an admin notice.
-		add_action('admin_notices', array($this, 'admin_notice_missing_main_plugin'));
-		return false;
-	}
-
-    /**
-	* Show notice to enable Elementor Pro
-	*/
-	public function admin_notice_missing_main_plugin() {
-		$message = sprintf(
-				// translators: %1$s replace with Form Input Masks for Elementor Form & %2$s replace with Elementor Pro.
-				esc_html__(
-					'%1$s requires %2$s to be installed and activated.',
-					'form-masks-for-elementor'
-				),
-				esc_html__( 'Form Input Masks for Elementor Form', 'form-masks-for-elementor' ),
-				esc_html__( 'Elementor Pro', 'form-masks-for-elementor' ),
-			);
-			printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', esc_html( $message ) );
-			deactivate_plugins( plugin_basename( __FILE__ ) );
 	}
 
     public function text_domain_path_set(){
